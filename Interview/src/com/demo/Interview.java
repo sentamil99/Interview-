268d3c1ea2a5aa155b6c690c26a15525438421c1
sentamil <selvansenthamil095.com>
2023-02-07 21:43:46 +0530

    multiple3and5();,multiple3OR5();,divided100(); ,

--- Interview/src/com/demo/Interview.java
+++ Interview/src/com/demo/Interview.java
@@ -7,7 +7,43 @@ public class Interview {
 		Interview in = new Interview();
 		//in.primeorNo();
 		//in.reverseNumber();
-		in.factorial(5);
+		//in.factorial(5);
+		//in.multiple3and5();
+		//in. multiple3or5();
+		in.divide100();
+	}
+	private void divide100() {
+		// TODO Auto-generated method stub
+		int number=100;
+		int div=2;
+		while(number>div) { 
+			if(number%div==0) {
+				System.out.print(div+" ");
+			}
+			div=div+1;
+		}
+		System.out.println();
+	}
+	private static void multiple3or5() {
+		// TODO Auto-generated method stub
+		int strat=3;
+		while(strat<=30) {
+			if(strat%3==0 || strat%5==0) {
+				System.out.print(strat+" ");
+			}strat=strat+1;
+		}
+		System.out.println();
+	}
+	private void multiple3and5() {
+		// TODO Auto-generated method stub
+		int strat=3;
+		while(strat<=30) {
+			if(strat%3==0 && strat%5==0) {
+				System.out.print(strat+" ");
+			}
+			strat=strat+1;
+		}
+		System.out.println();
 	}
 	private int factorial(int fact) {
 		int count=1;
